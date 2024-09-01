046a57f0610a139f3cf64fc27c397342bf33e6d3
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-02 02:23:47 +0300

    refactor: refine  error-middleware

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -1,7 +1,13 @@
 /* eslint-disable no-console */
 import { NextFunction, Request, Response } from 'express';
 
-import { ApiError } from '#/errors/api-error';
+import {
+  ActivationCodeIncorrect,
+  ActivationError,
+  ActivationMaxAttemptsExceededError,
+  ActivationRateLimitError,
+  ApiError
+} from '#/errors/api-error';
 import { Prisma } from '@prisma/client';
 import { CONFIG } from '#config';
 
@@ -18,24 +24,42 @@ export function ErrorMiddleware(
   res: Response,
   next: NextFunction
 ) {
-  let message = 'Something went wrong...';
   let status = 500;
-  let name = 'InternalError';
+
+  const defaultMessage = 'Something went wrong...';
+  const defaultName = 'InternalError';
+
+  const body: Record<string, string | number | string[]> = {
+    name: defaultName,
+    message: defaultMessage
+  };
 
   if (error instanceof ApiError) {
     status = error.status;
-    if (CONFIG.NODE_ENV !== 'production') {
-      name = error.name;
-      message = error.message;
-    } else if (!isDatabaseError(error)) {
-      name = error.name;
-      message = error.message;
+    body.message = error.message;
+    body.name = error.name;
+
+    if (error instanceof ActivationError) {
+      if (error instanceof ActivationCodeIncorrect) {
+        body.attemptsLeft = error.attemptsLeft;
+      } else if (
+        error instanceof ActivationRateLimitError ||
+        error instanceof ActivationMaxAttemptsExceededError
+      ) {
+        body.secondsUntilNextCode = error.secondsUntilNextCode;
+        res.setHeader('Retry-After', error.secondsUntilNextCode.toString());
+      }
     }
+  } else if (isDatabaseError(error)) {
+    status = 500;
+    body.name = defaultName;
+    body.message = defaultMessage;
   }
+
   if (CONFIG.NODE_ENV !== 'production') {
     if (error instanceof ApiError) console.error('Operational error: ', error);
     else console.error('Non-operational error: ', error);
   }
 
-  return res.status(status).json({ message, name });
+  return res.status(status).json(body);
 }
