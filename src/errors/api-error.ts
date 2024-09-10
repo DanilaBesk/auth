2e99e04a5243cdb06633debcd0ed515b1d9d906b
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-11 00:04:19 +0300

    feat: add new access token, refresh sessions, user, route not found error-classes in api-error.ts

--- src/errors/api-error.ts
+++ src/errors/api-error.ts
@@ -1,3 +1,5 @@
+import { MAX_REFRESH_TOKENS_FOR_USER } from '#/constants/auth.constants';
+
 export class ApiError extends Error {
   status: number;
 
@@ -17,18 +19,104 @@ export class ApiError extends Error {
 }
 
 export class BadRequestError extends ApiError {
-  constructor({ message, cause }: { message: string; cause?: unknown }) {
-    super({ status: 400, message, cause });
+  constructor({ message }: { message: string }) {
+    super({ status: 400, message });
   }
 }
 export class UnauthorizedError extends ApiError {
-  constructor({ message, cause }: { message: string; cause?: unknown }) {
-    super({ status: 401, message, cause });
+  constructor({ message }: { message: string }) {
+    super({ status: 401, message });
+  }
+}
+export class AccessTokenExpiredError extends ApiError {
+  expiredAt: Date;
+
+  constructor({ expiredAt }: { expiredAt: Date }) {
+    super({ message: 'Access token expired', status: 419 });
+    this.expiredAt = expiredAt;
+  }
+}
+
+export class AccessTokenVerifyError extends UnauthorizedError {
+  constructor() {
+    super({ message: 'Access token verify error' });
+  }
+}
+
+export class RefreshSessionNotFoundOrExpiredError extends UnauthorizedError {
+  constructor() {
+    super({
+      message: 'Refresh session not found or has expired'
+    });
+  }
+}
+export class RefreshSessionInvalidError extends UnauthorizedError {
+  constructor() {
+    super({
+      message: 'Invalid session. Wrong fingerprint'
+    });
+  }
+}
+
+export class MaxRefreshSessionsExceededError extends ApiError {
+  constructor() {
+    super({
+      status: 429,
+      message: `Cannot create more than ${MAX_REFRESH_TOKENS_FOR_USER} sessions`
+    });
+  }
+}
+
+export class InvalidPasswordError extends UnauthorizedError {
+  constructor() {
+    super({
+      message: 'Invalid password'
+    });
+  }
+}
+
+export class UserEmailConflictError extends ApiError {
+  constructor() {
+    super({
+      status: 409,
+      message: 'User already exists with this email address'
+    });
+  }
+}
+
+export class UserEmailNotFoundError extends ApiError {
+  constructor() {
+    super({
+      status: 404,
+      message: 'User not found with this email address'
+    });
+  }
+}
+
+export class UserIdNotFoundError extends ApiError {
+  constructor() {
+    super({
+      status: 404,
+      message: 'User not found with this ID'
+    });
+  }
+}
+
+export class RouteNotFoundError extends ApiError {
+  url: string;
+
+  constructor({ url }: { url: string }) {
+    super({
+      status: 404,
+      message: `Route '${url}' not found`
+    });
+    this.url = url;
   }
 }
 
 export class ValidationError extends ApiError {
   errors: string[];
+
   constructor({ errors, cause }: { errors: string[]; cause?: unknown }) {
     super({ status: 400, cause, message: 'ValidationError' });
     this.errors = errors;
@@ -45,7 +133,7 @@ export class ActivationMaxAttemptsExceededError extends ActivationError {
   constructor({ secondsUntilNextCode }: { secondsUntilNextCode: number }) {
     super({
       status: 429,
-      message: 'No attempts left, request a new code'
+      message: `No attempts left. Please wait ${secondsUntilNextCode} seconds before requesting a new code`
     });
     this.secondsUntilNextCode = secondsUntilNextCode;
   }
