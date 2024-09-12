ac6b06d5f25b1f4676687e2becbd5c050ed84e72
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-12 21:26:36 +0300

    refactor: add index file to providers folder

---
+++ src/providers/index.ts
@@ -0,0 +1,4 @@
+export { ipdata } from '#/providers/ipdata.provider';
+export { mailTransporter } from '#/providers/mail-transporter.provider';
+export { prisma } from '#/providers/prisma.provider';
+export { redis } from '#/providers/redis.provider';
