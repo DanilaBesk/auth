63829f28413c90c545542d388885f5e4a93bfa5f
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-12 21:01:28 +0300

    feat: add a lot contants for app in auth.constants.ts

--- src/constants/auth.constants.ts
+++ src/constants/auth.constants.ts
@@ -1,6 +1,33 @@
-export const MAX_REFRESH_TOKENS_FOR_USER = 5;
-export const MAX_PASSWORD_SYMBOLS = 20;
+import { CookieOptions } from 'express';
+import { CONFIG } from '#config';
+import { Role } from '@prisma/client';
 
+export const MAX_REFRESH_TOKENS_FOR_USER = 5;
+export const MIN_PASSWORD_LENGTH = 8;
+export const MAX_PASSWORD_LENGTH = 30;
+
+export const ACTIVATION_CODE_LENGTH = 6;
 export const ACTIVATION_CODE_EXPIRE_IN = 60 * 5;
 export const ACTIVATION_CODE_ATTEMPTS_LIMIT = 3;
 export const ACTIVATION_CODE_REQUEST_INTERVAL_SECONDS = 60;
+
+export const CHANGE_EMAIL_CODE_LENGTH = 6;
+
+export const CHANGE_PASSWORD_CODE_LENGTH = 6;
+
+export const BCRYPT_SALT_ROUNDS = 10;
+
+export const ACCESS_TOKEN_EXPIRES_IN = 5 * 60;
+export const REFRESH_TOKEN_EXPIRES_IN = 14 * 24 * 60 * 60;
+
+export const INITIAL_USER_ROLE = Role.USER;
+
+export const REFRESH_COOKIE_OPIONS: CookieOptions = {
+  maxAge: REFRESH_TOKEN_EXPIRES_IN * 1000,
+  path: '/api/auth',
+  domain: CONFIG.CLIENT_URL,
+  secure: false, //TODO: chage to true
+  httpOnly: true,
+  sameSite: 'strict',
+  signed: true
+};
