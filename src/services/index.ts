94a18d0026917ea0c0b7ec612c57abb8865c107b
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-14 23:52:31 +0300

    refactor: add index file to services

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export { UserService } from '#/services/user.service';
+export { AuthService } from '#/services/auth.service';
+export { TokenService } from '#/services/token.service';
+export { MailService } from '#/services/mail.service';
