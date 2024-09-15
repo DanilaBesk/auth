1f8ead1348e7878b7bbdb4f97f7a23d9554bedbf
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-15 15:22:38 +0300

    feat: add validate-token-payload.utility and rename validateData function to validateRequestData

--- src/utils/validate-request-data.utility.ts
+++ src/utils/validate-request-data.utility.ts
@@ -1,11 +1,15 @@
 import { Request } from 'express';
-import { AnyZodObject, z } from 'zod';
+import { z } from 'zod';
+
 import { ValidationError } from '#/errors/api-error';
 
-export const validateData = async <T extends AnyZodObject>(
-  schema: T,
-  req: Request
-): Promise<z.infer<T>> => {
+export const validateRequestData = async <T extends z.AnyZodObject>({
+  schema,
+  req
+}: {
+  schema: T;
+  req: Request;
+}): Promise<z.infer<T>> => {
   const result = await schema.safeParseAsync(req);
 
   if (!result.success) {
---
+++ src/utils/validate-token-payload.utility.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+import { InvalidTokenPayloadError } from '#/errors/api-error';
+import { TTokenType } from '#/types/token.types';
+
+export const validateTokenPayload = async <T extends z.AnyZodObject>({
+  schema,
+  payload,
+  tokenType
+}: {
+  schema: T;
+  payload: unknown;
+  tokenType: TTokenType;
+}): Promise<z.infer<T>> => {
+  const result = await schema.safeParseAsync(payload);
+
+  if (!result.success) {
+    throw new InvalidTokenPayloadError({ tokenType });
+  }
+
+  return result.data;
+};
