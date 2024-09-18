e230dbf1b4a530ee796db172260d5752226ddf6a
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-18 23:29:54 +0300

    feat: add new token types for token service

--- src/types/token.types.ts
+++ src/types/token.types.ts
@@ -1,35 +1,94 @@
+import { z } from 'zod';
 import { User } from '@prisma/client';
 
-export type TUserId = User['id'];
-export type TRefreshUUID = string;
+import {
+  AccessTokenPayloadSchema,
+  RefreshTokenPayloadSchema
+} from '#/schemas/token.schemas';
 
-export type TAccessPayload = Pick<User, 'role' | 'email'> & { sub: TUserId };
-
-export type TMakeAccessToken = Pick<User, 'email' | 'id' | 'role'>;
+export type TTokenType = 'access' | 'refresh';
 
 export type TRefreshSession = {
+  tokenSignature: string;
   ip: string;
   ua: string;
   fingerprint: string;
-  expiresAt: number;
+  createdAt: number;
 };
+
+export type TValidateTokenPayload<T extends z.AnyZodObject> = {
+  schema: T;
+  payload: unknown;
+  tokenType: TTokenType;
+};
+
+export type TJwtSign = {
+  payload: object;
+  secret: string;
+  subject: string;
+  expiresIn: number;
+};
+
+export type TJwtVerify<T extends z.AnyZodObject> = {
+  tokenType: TTokenType;
+  token: string;
+  schema: T;
+  secret: string;
+};
+
+export type TJwtDecode = {
+  token: string;
+};
+
+export type TGetRefreshSessionKey = { userId: User['id'] };
+
+type TAccessTokenPayloadSchema = z.infer<typeof AccessTokenPayloadSchema>;
+
+type TRefreshTokenPayloadSchema = z.infer<typeof RefreshTokenPayloadSchema>;
+
+export type TAccessTokenData = Omit<TAccessTokenPayloadSchema, 'sub'> & {
+  userId: TAccessTokenPayloadSchema['sub'];
+};
+
+export type TRefreshTokenData = Omit<TRefreshTokenPayloadSchema, 'sub'> & {
+  userId: TRefreshTokenPayloadSchema['sub'];
+};
+
+export type TMakeAccessToken = TAccessTokenData;
+
+export type TMakeRefreshTokenData = Pick<TRefreshTokenData, 'userId'>;
 
 export type TAddRefreshSession = {
-  userId: TUserId;
-  refreshUUID: TRefreshUUID;
-} & Omit<TRefreshSession, 'expiresAt'>;
-
-export type TRemoveRefreshSession = {
-  userId: TUserId;
-  refreshUUID: TRefreshUUID;
-};
+  userId: User['id'];
+  refreshSessionId: string;
+} & Omit<TRefreshSession, 'createdAt'>;
 
 export type TGetRefreshSession = {
-  userId: TUserId;
-  refreshUUID: TRefreshUUID;
+  userId: User['id'];
+  refreshSessionId: string;
 };
 
-export type TVerifyRefreshSession = {
-  oldRefreshSession: TRefreshSession;
-  newFingerprint: TRefreshSession['fingerprint'];
+export type TGetAllUserRefreshSessions = {
+  userId: User['id'];
+};
+
+export type TGetUserRefreshSessionsCount = {
+  userId: User['id'];
+};
+
+export type TDeleteRefreshSession = {
+  userId: User['id'];
+  refreshSessionId: string;
+};
+
+export type TDeleteAllUserRefreshSessions = {
+  userId: User['id'];
+};
+
+export type TVerifyRefreshToken = {
+  refreshToken: string;
+};
+
+export type TVerifyAccessToken = {
+  accessToken: string;
 };
