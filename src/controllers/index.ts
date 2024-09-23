3982df40a5ab21b8efee4823532d77e049036141
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-23 23:06:46 +0300

    refactor: add index.ts file for controllers folder

---
+++ src/controllers/index.ts
@@ -0,0 +1,3 @@
+export { BaseController } from '#/controllers/base.controller';
+export { UserController } from '#/controllers/user.controller';
+export { AuthController } from '#/controllers/auth.controller';
