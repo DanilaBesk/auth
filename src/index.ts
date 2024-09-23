7912557a123069d29eaf510e1d36fc7afe618d9f
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-23 23:59:29 +0300

    feat: update src/index.ts file, add router, process SIGINT, SIGTERM

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 /* eslint-disable no-console */
-import { config } from 'dotenv';
+
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application } from 'express';
 
-config({ path: '../.env' });
-
-import { ErrorMiddleware } from '#/middlewares/error.middleware';
-import { prisma } from '#/providers/prisma.provider';
 import { CONFIG } from '#config';
-import { RouteNotFoundMiddleware } from '#/middlewares/route-not-found.middleware';
+import { prisma, redis } from '#/providers';
+import { router } from '#/routes';
+import { RouteNotFoundMiddleware, ErrorMiddleware } from '#/middlewares';
 
 export const app: Application = express();
 
@@ -26,6 +24,8 @@ app.use(
 app.use(express.json());
 app.use(cookieParser(CONFIG.COOKIE_SECRET));
 
+app.use('/api', router);
+
 app.all('*', RouteNotFoundMiddleware);
 
 app.use(ErrorMiddleware);
@@ -33,6 +33,7 @@ app.use(ErrorMiddleware);
 const start = async () => {
   try {
     await prisma.$connect();
+    await redis.connect();
     app.listen(CONFIG.APP_PORT, CONFIG.APP_HOST, () => {
       console.info(
         `Server start on:
@@ -44,4 +45,14 @@ const start = async () => {
     console.error(error);
   }
 };
+
+const handleProcessCompletion = async () => {
+  await prisma.$disconnect();
+  await redis.quit();
+  process.exit(0);
+};
+
+process.on('SIGINT', handleProcessCompletion);
+process.on('SIGTERM', handleProcessCompletion);
+
 start();
