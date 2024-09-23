faeccc4bf62e9a14c1d3a25d630f455efc32666c
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-23 23:55:06 +0300

    feat: add hot reloading functionality to redis client

--- src/providers/redis.provider.ts
+++ src/providers/redis.provider.ts
@@ -1,10 +1,22 @@
 /* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 import { createClient } from 'redis';
 import { CONFIG } from '#config';
 
-export const redis = await createClient({
-  url: CONFIG.REDIS_URL,
-  socket: { tls: true }
-})
-  .on('error', (err) => console.log('Redis Client Error', err))
-  .connect();
+declare global {
+  var redis: ReturnType<typeof createClient> | undefined;
+}
+
+const redis =
+  global.redis ||
+  createClient({
+    url: CONFIG.REDIS_URL,
+    socket: { tls: true }
+  }).on('error', (err) => console.log('Redis Client Error', err));
+
+if (process.env.NODE_ENV !== 'production') {
+  global.redis = redis;
+}
+
+export { redis };
