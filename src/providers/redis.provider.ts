44b9d0ad2bd4a9fa49ba88e31d4ecf81be03c7bc
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-24 22:57:18 +0300

    fix: remove option 'socket' from redis provider due to Redis database change

--- src/providers/redis.provider.ts
+++ src/providers/redis.provider.ts
@@ -11,8 +11,7 @@ declare global {
 const redis =
   global.redis ||
   createClient({
-    url: CONFIG.REDIS_URL,
-    socket: { tls: true }
+    url: CONFIG.REDIS_URL
   }).on('error', (err) => console.log('Redis Client Error', err));
 
 if (process.env.NODE_ENV !== 'production') {
