6fb395e4d16cb00a1d67f43b6ddc9332a2ec63d3
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-28 19:53:17 +0300

    chore: remove unused general-utils.types.ts and zod-utils.types.ts files

--- src/types/general-utils.types.ts
+++
@@ -1,5 +0,0 @@
-export type UnionToIntersection<U> = (
-  U extends any ? (_: U) => void : never
-) extends (_: infer I) => void
-  ? I
-  : never;
--- src/types/zod-utils.types.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-import { UnionToIntersection } from '#/types/general-utils.types';
-
-export type FlattenSchemaTypes<T extends z.ZodTypeAny> = UnionToIntersection<
-  z.infer<T>[keyof z.infer<T>]
->;
