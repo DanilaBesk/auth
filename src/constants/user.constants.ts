3434e68b8db2f895ebb714da3d4e525bb066458e
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-09-29 13:54:01 +0300

    feat: improve architecture for timeouts and reset of activation code entry

--- src/constants/user.constants.ts
+++ src/constants/user.constants.ts
@@ -3,13 +3,14 @@ import { Role } from '@prisma/client';
 export const ACTIVATION_CODE_LENGTH = 6;
 export const ACTIVATION_CODE_EXPIRATION_TIME_SECONDS = 60 * 5;
 export const ACTIVATION_CODE_ATTEMPTS_LIMIT = 3;
-export const ACTIVATION_CODE_REQUEST_INTERVAL_SECONDS = [
-  0,
-  60,
-  2 * 60,
-  5 * 60,
-  2 * 60 * 60
-];
+export const ACTIVATION_CODE_REQUEST_INTERVALS_SECONDS: Record<number, number> =
+  {
+    1: 0,
+    2: 60,
+    3: 2 * 60,
+    4: 5 * 60
+  };
+export const ACTIVATION_CODE_RESET_TTL_SECONDS = 2 * 60 * 60;
 
 export const CHANGE_EMAIL_CODE_LENGTH = 6;
 
