79d9944acf22edb88bfb3f94245183d3d970ea5d
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-10-06 23:04:41 +0300

    refactor: add CodeService to index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,4 @@ export { UserService } from '#/services/user.service';
 export { AuthService } from '#/services/auth.service';
 export { TokenService } from '#/services/token.service';
 export { MailService } from '#/services/mail.service';
+export { CodeService } from '#/services/code.service';
