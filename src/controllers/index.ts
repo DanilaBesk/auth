ca40696c73a14b4e88e6595dec773c2de0db4d17
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-10-25 00:17:09 +0300

    refactor: remove unused controller export

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,2 @@
-export { BaseController } from '#/controllers/base.controller';
-export { UserController } from '#/controllers/user.controller';
 export { AuthController } from '#/controllers/auth.controller';
+export { UserController } from '#/controllers/user.controller';
