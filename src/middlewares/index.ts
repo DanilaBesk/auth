54b1134ee47f2f58b4a1ef26f9c385f6910d98eb
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-10-25 00:18:07 +0300

    refactor: add some middleware exports to index.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,5 @@ export { ErrorMiddleware } from '#/middlewares/error.middleware';
 export { RouteNotFoundMiddleware } from '#/middlewares/route-not-found.middleware';
 export { JsonParseMiddleware } from '#/middlewares/json-parse.middleware';
 export { CookieParseMiddleware } from '#/middlewares/cookie-parse.middleware';
+export { FileUploadMiddleware } from '#/middlewares/file-upload.middleware';
+export { CheckAccessTokenMiddleware } from '#/middlewares/check-access-token.middleware';
