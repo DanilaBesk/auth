65101f7ce0dece7f5bfd8d94f90784b2a2997bdb
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-10-25 10:10:50 +0300

    refactor: add avatar routes to index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { userRouter } from '#/routes/user.routes';
 import { authRouter } from '#/routes/auth.routes';
+import { avatarRouter } from '#/routes/avatar.routes';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
+router.use('/avatars', avatarRouter);
 
 export { router };
